d3ee0afa64bfc0c0fb791f3cbcd4c63a2bcfdb04
Federico Reina <federicoreinavinchira@yahoo.com>
2020-10-28 19:09:25 -0500

    Updates

---
+++ precache-manifest.b6881de527a3408cb482fbd18d95298f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea2537bbe1c55ca76a1634c4286dc0f",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "2be0f828d7e4f8ef3fb9",
+    "url": "/TodoApp/static/css/main.b4d639b3.chunk.css"
+  },
+  {
+    "revision": "d9934dfff1c49079188c",
+    "url": "/TodoApp/static/js/2.6a13ab0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.6a13ab0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be0f828d7e4f8ef3fb9",
+    "url": "/TodoApp/static/js/main.d4ae396c.chunk.js"
+  },
+  {
+    "revision": "6b7debdcb68d45e6d650",
+    "url": "/TodoApp/static/js/runtime-main.bbf2a0aa.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/TodoApp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
